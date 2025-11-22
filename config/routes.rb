76b7bcf9bbe451bd9822068b21c9889ee46a62f0
y-sakurai <235877267+y-sakurai-code@users.noreply.github.com>
2025-11-22 09:47:09 +0000

    routes fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get 'homes/top'
-  get 'homes/about'
+  resources :users, only: [:index, :show, :edit]
+  resources :books, only: [:new, :index, :show, :edit]
+  root to: "homes#top"
+  get '/homes/about', to: 'homes#about', as: 'about'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
