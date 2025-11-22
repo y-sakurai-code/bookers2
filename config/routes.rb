0e942954ff4f35f03296e9699c745fe03b075606
y-sakurai <235877267+y-sakurai-code@users.noreply.github.com>
2025-11-22 11:57:51 +0000

    make 動的なヘッダー

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :users, only: [:index, :show, :edit]
   resources :books, only: [:new, :index, :show, :edit]
   root to: "homes#top"
   get '/homes/about', to: 'homes#about', as: 'about'
-  devise_for :users
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
