d31bfc15d6a7621a681527365a6e7a1b6eb09d20
y-sakurai <235877267+y-sakurai-code@users.noreply.github.com>
2025-11-27 12:16:23 +0000

    feat/books_index.html add book_table

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -17,8 +17,9 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
+    @book.user_id = current_user.id
     @book.save
-    redirect_to '/books'
+    redirect_to books_path
   end
 
   private
