9e269bf4dd4f138b06396a8943a6e937148e4141
y-sakurai <235877267+y-sakurai-code@users.noreply.github.com>
2025-12-01 12:41:34 +0000

    fixes/books_cotroller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,14 +19,23 @@ class BooksController < ApplicationController
   def create
     @book_new = Book.new(book_params)
     @book_new.user_id = current_user.id
-    @book_new.save
-    redirect_to books_path
+    if @book_new.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to books_path
+    else
+      @book_all = Book.all
+      render :new
+    end
   end
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book.id)
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render :new
+    end
   end
 
   def destroy
